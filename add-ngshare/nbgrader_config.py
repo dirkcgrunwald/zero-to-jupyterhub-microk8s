26702816352b9a56cbb9090af723dd0fbb558523
Dirk Grunwald <grunwald@colorado.edu>
2020-07-09 10:59:26 -0600

    add ngshare extension

---
+++ add-ngshare/nbgrader_config.py
@@ -0,0 +1,6 @@
+from ngshare_exchange import configureExchange
+c=get_config()
+configureExchange(c, 'http://ngshare.default.svc.cluster.local:8080/services/ngshare')
+# Add the following line to let students access courses without configuration
+# For more information, read Notes for Instructors in the documentation
+c.CourseDirectory.course_id = '*'
